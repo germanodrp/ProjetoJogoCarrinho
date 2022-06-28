353c341a763fee5a4186669d5e6abaed50bcd10b
JeeFzPiires <jeferson.piires@gmail.com>
2022-06-28 17:38:53 -0300

    correção - frase instruções

--- src/components/ModalAjuda/index.jsx
+++ src/components/ModalAjuda/index.jsx
@@ -19,7 +19,7 @@ export function ModalAjuda({ onClose }) {
                 <Content>
                     <h1>INSTRUÇÕES</h1><br/><br/><br/><br/>
                     <h2>Memorize as imagens do carrinho a sua direita</h2><br/><br/>
-                    <h2>Clique na prateleira a imagem que correspondem a do carrinho.</h2><br/><br/>
+                    <h2>Clique na prateleira nas imagens correspondentes as do carrinho.</h2><br/><br/>
                     <h2>Selecione as imagens até que os carrinhos fiquem iguais.</h2>
                 </Content>
             </Container>
