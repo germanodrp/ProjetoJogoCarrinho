790a36b72d239abee018ba1b16edc2058d30ffbf
JeeFzPiires <jeferson.piires@gmail.com>
2022-06-29 20:40:23 -0300

    Finalizando 2

--- src/components/SegundoCarrinho/styles.js
+++ src/components/SegundoCarrinho/styles.js
@@ -19,7 +19,7 @@ export const Content = styled.div`
 export const Container = styled.div`
 
     display: flex;
-    margin-right: 50px;
+    margin-right: -40%;
     
     img {
         width: 14.063rem;
